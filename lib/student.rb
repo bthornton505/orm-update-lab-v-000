8f35e770463dedb1202c089540ddecc3ad8802fb
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-15 23:19:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,8 +44,10 @@ class Student
     end 
   end 
   
-  def self.create 
-    student = Student.new()
+  def self.create(name:, grade;)
+    student = Student.new(name, grade)
+    student.save
+    student 
   end 
   
   # def self.new_from_db(row)
