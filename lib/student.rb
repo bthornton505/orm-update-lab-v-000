95105bc00aff489b0cdf8c75a2efe59db2015203
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-15 22:51:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,11 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade, :id 
-
+  
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade 
+    @id = id 
+  end 
+  
 end
